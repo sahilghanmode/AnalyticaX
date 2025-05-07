3bac59896842948b103edb4a213503f7910af9b7
sahilghanmode <sahilghanmode1903@gmail.com>
2025-05-07 17:43:55 +0530

    small change

--- frontend/src/pages/auth/Verification.jsx
+++ frontend/src/pages/auth/Verification.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Verification = () => {
   return (
-    <div>
+    <div className='fixed left-0 top-0 bg-black/50 flex justify-center items-center h-[100dvh] w-full'>
       this is verify
     </div>
   )
